1b7d2871143a53b135b9ba42df47ff5d438e3c54
viniciusmalloc <vini.martins.7@gmail.com>
2015-12-06 11:01:59 -0200

    tays chata

--- src/produtorconsumidor/MainGerenciadorBuffer.java
+++ src/produtorconsumidor/MainGerenciadorBuffer.java
@@ -5,6 +5,11 @@ package produtorconsumidor;
  * @author viniciuscoelho, thaismombach
  */
 
+/*
+thaismombach@192.168.25.6
+java produtorconsumidor/MainGerenciadorBuffer 3 192.168.25.10 12347 192.168.25.6 12348 192.168.25.10 12349
+*/
+
 public class MainGerenciadorBuffer {
 
     public static void main(String[] args) {
@@ -16,9 +21,9 @@ public class MainGerenciadorBuffer {
             int n = Integer.parseInt(args[0]);
             String [] addresses = new String[args.length/2];
             int [] ports = new int[args.length/2];
-            for ( int i = 1; i < args.length; i += 2 ) {
-                addresses[i-1] = args[i];
-                ports[i-1] = Integer.parseInt(args[i+1]);
+            for ( int i = 0, j = 1; i < n; i++, j += 2 ) {
+                addresses[i] = args[j];
+                ports[i] = Integer.parseInt(args[j+1]);
             }
             GerenciadorBuffer g = new GerenciadorBuffer(n, addresses, ports);
             g.waitForConnections();
